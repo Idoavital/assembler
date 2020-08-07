ad8c00279c4be80ce60ce6f3731357671afcd400
עידו אביטל <ido-avital@hotmail.com>
2020-08-07 12:53:08 +0300

    clear line

--- assembler/Data_structures.c
+++ assembler/Data_structures.c
@@ -8,7 +8,6 @@
 char* g_keywords[MAX_KEYWORDS] = { ".extern", ".entry", ".data", ".string", "mov", "cmp", "add", "sub", "lea", "clr", "not",
 					"inc", "dec", "jmp", "bne", "jsr", "red", "prn", "rts", "stop", "r0", "r1", "r2", "r3", "r4", "r5", "r6", "r7" };
 
-
 /*contains the divided line. every row represent a word in the sentence. */
 char splitLine [MAX_LINE_LEN][MAX_LINE_LEN];
 /*The current line number that read in the assembly file (for print error) */
